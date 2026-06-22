11008ed09fcfbe73192f4c0e050d97482752ddec
Abhijeet chandra <chandraabhijeet475@gmail.com>
2026-06-22 20:10:21 +0530

    Added_log_Monitoring_function

---
+++ monitor.py
@@ -0,0 +1,39 @@
+import os
+import time
+
+Log_file = "logs/alerts.log"
+
+def main():
+    monitor_logs()
+
+
+def monitor_logs():
+    print("checking Log file")
+
+    if not os.path.exists(Log_file):
+        print(f"{Log_file} is not available")
+        return
+    
+    with open(Log_file, "r")as file:
+        lines = file.readlines()
+    
+
+    error_detected = False
+
+    for line in lines:
+        if "[error]" in line.lower() and "down"  in line.lower():
+            print(f"\n Alert Detected {line.strip()}")
+            error_detected = True
+            break
+
+    if error_detected:
+        print("Restarting service")
+
+    else:
+        print("System is fine and successful able to reach server")
+        
+
+if __name__ == "__main__":
+    main()
+
+
